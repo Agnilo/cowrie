51ea79e41ec2e392228634cd40b10c80dfcd2840
Agnilo <agne.butkute99@gmail.com>
2024-12-17 19:42:11 +0200

    changes

--- src/cowrie/core/checkers copy.py
+++ src/cowrie/core/checkers copy.py
@@ -19,10 +19,9 @@ from twisted.cred.error import UnauthorizedLogin, UnhandledCredentials
 from twisted.internet import defer
 from twisted.python import failure, log
 
-from cowrie.core import auth
 from cowrie.core import credentials as conchcredentials
 from cowrie.core.config import CowrieConfig
-from cowrie.shell.protocol import HoneyPotBaseProtocol
+import cowrie.core.auth  # noqa: F401
 
 
 @implementer(ICredentialsChecker)
@@ -44,7 +43,26 @@ class HoneypotPublicKeyChecker:
             type=_pubKey.sshType(),
         )
 
-        return failure.Failure(error.ConchError("Incorrect signature"))
+        if CowrieConfig.getboolean("ssh", "auth_publickey_allow_any", fallback=False):
+            log.msg(
+                eventid="cowrie.login.success",
+                format="public key login attempt for [%(username)s] succeeded",
+                username=credentials.username,
+                fingerprint=_pubKey.fingerprint(),
+                key=_pubKey.toString("OPENSSH"),
+                type=_pubKey.sshType(),
+            )
+            return defer.succeed(credentials.username)
+        else:
+            log.msg(
+                eventid="cowrie.login.failed",
+                format="public key login attempt for [%(username)s] failed",
+                username=credentials.username,
+                fingerprint=_pubKey.fingerprint(),
+                key=_pubKey.toString("OPENSSH"),
+                type=_pubKey.sshType(),
+            )
+            return failure.Failure(error.ConchError("Incorrect signature"))
 
 
 @implementer(ICredentialsChecker)
@@ -56,6 +74,11 @@ class HoneypotNoneChecker:
     credentialInterfaces = (conchcredentials.IUsername,)
 
     def requestAvatarId(self, credentials):
+        log.msg(
+            eventid="cowrie.login.success",
+            format="login attempt [%(username)s] succeeded",
+            username=credentials.username,
+        )
         return defer.succeed(credentials.username)
 
 
@@ -71,12 +94,10 @@ class HoneypotPasswordChecker:
     )
 
     def requestAvatarId(self, credentials):
-
-        protocol = getattr(credentials, "protocol", None)
-        log.msg(f"Passing protocol: {protocol} for user: {credentials.username}")
-
         if hasattr(credentials, "password"):
-            if self.checkUserPass(credentials.username, credentials.password, credentials.ip, protocol):
+            if self.checkUserPass(
+                credentials.username, credentials.password, credentials.ip
+            ):
                 return defer.succeed(credentials.username)
             return defer.fail(UnauthorizedLogin())
         if hasattr(credentials, "pamConversion"):
@@ -95,27 +116,20 @@ class HoneypotPasswordChecker:
                 return defer.succeed(username)
         return defer.fail(UnauthorizedLogin())
 
-    def checkUserPass(self, theusername: bytes, thepassword: bytes, ip: str, protocol=None) -> bool:
-        # UserDB is the default auth_class
-        log.msg(f"Protocol passed: {protocol} for user: {theusername.decode('utf-8')}")
-
-
-        authname = auth.UserDB
-
+    def checkUserPass(self, theusername: bytes, thepassword: bytes, ip: str) -> bool:
         # Is the auth_class defined in the config file?
-        if CowrieConfig.has_option("honeypot", "auth_class"):
-            authclass = CowrieConfig.get("honeypot", "auth_class")
-            authmodule = "cowrie.core.auth"
+        authclass = CowrieConfig.get("honeypot", "auth_class", fallback="UserDB")
+        authmodule = "cowrie.core.auth"
 
-            # Check if authclass exists in this module
-            if hasattr(modules[authmodule], authclass):
-                authname = getattr(modules[authmodule], authclass)
-            else:
-                log.msg(f"auth_class: {authclass} not found in {authmodule}")
+        # Check if authclass exists in this module
+        if hasattr(modules[authmodule], authclass):
+            authname = getattr(modules[authmodule], authclass)
+        else:
+            log.msg(f"auth_class: {authclass} not found in {authmodule}")
 
         theauth = authname()
 
-        if theauth.checklogin(theusername, thepassword, ip, protocol):
+        if theauth.checklogin(theusername, thepassword, ip):
             log.msg(
                 eventid="cowrie.login.success",
                 format="login attempt [%(username)s/%(password)s] succeeded",
